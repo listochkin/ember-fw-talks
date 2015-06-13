d22cfb5211cd5299390f6947120784cf4638ed8b
Andrey Listochkin <listochkin@outlook.com>
2015-06-13 15:41:13 +0300

    protect events/new route

--- app/event-list/event/new/route.js
+++ app/event-list/event/new/route.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
   actions: {
     saveEvent (event) {
       const eventModel = this.store.createRecord('event', event);
