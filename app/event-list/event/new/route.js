b71f02bb48c4791a5e5d126c25405909f308fee6
Andrey Listochkin <listochkin@outlook.com>
2015-06-13 13:25:29 +0300

    save new event from route

--- app/event-list/event/new/route.js
+++ app/event-list/event/new/route.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    saveEvent (event) {
+      const eventModel = this.store.createRecord('event', event);
+      eventModel.save();
+    }
+  }
 });
