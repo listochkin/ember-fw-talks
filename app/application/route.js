53890535adf3e779df4e35c07cb69e0dca1e7e50
Andrey Listochkin <listochkin@outlook.com>
2015-06-13 15:22:25 +0300

    moved login action handler to application route (BUG)

--- app/application/route.js
+++ app/application/route.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  actions: {
+    authenticateSession () {
+      this.get('session').authenticate('authenticator:firebase', 'twitter');
+    }
+  }
 });
--- app/login/route.js
+++ app/login/route.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    authenticateSession () {
-      this.get('session').authenticate('authenticator:firebase', 'twitter');
-    }
-  }
 });
